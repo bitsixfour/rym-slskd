5963c5a6a6c38a92d2253b60d2cc1e0c52cf8938
B1tWasTaken <71984191+B1tWasTaken@users.noreply.github.com>
2025-12-27 17:34:20 -0600

    Add files via upload

---
+++ slskd.js
@@ -0,0 +1,95 @@
+import { getArtist, getTitle } from "./background.js";
+const apiKey = "dccee590-328f-4a16-a350-a728584eafd8";
+const url = "http://192.168.1.12:5030";
+
+async function slskdRequest() {
+  let request = fetch(url + "/api/v0/searches", {
+    method: "POST",
+    headers: {
+      "X-API-Key": apiKey,
+    },
+    body: JSON.stringify({
+      searchText: getArtist() + " " + getTitle(),
+    }),
+  });
+  let req = await request.json();
+  return req;
+}
+
+async function getId() {
+  return slskdRequest().id;
+}
+
+async function slskdStatus() {
+  let status = await fetch(url + "/api/v0/searches/" + getId(), {
+    method: "GET",
+    headers: {
+      "X-API-Key": apiKey,
+    },
+  });
+  let stringStatus = JSON.parse(status.json());
+  return stringStatus.isComplete();
+}
+
+function returnArray() {
+  return JSON.parse(fetch(url + "/api/v0/searches/" + getId() + "/responses"));
+}
+
+function directoryDownload(str) {
+  let newpath = str.split("\\\\").pop();
+  return newpath.join("/");
+}
+
+async function slskdDownload() {
+  while (true) {
+    const status = await slskdStatus();
+    if (status == true) {
+      break;
+    }
+    console.log("Waiting", 5000);
+  }
+  let arr = returnArray();
+  let target = 0;
+  let user = "";
+  for (let i = target; i < arr.length; i++) {
+    let obj = arr[i];
+    if (
+      obj.files[0].filename.includes("live") &&
+      (getArtist() + getTitle()).toLowerCase().includes("live") != true
+    ) {
+      continue;
+    }
+    for (let l = 0; l < obj.files.length; l++) {
+      if (
+        obj.filename.toLowercase().endsWith("flac") == true &&
+        obj.files[i].isLocked != false
+      ) {
+        target = i;
+        user = obj.username;
+        break;
+      }
+    }
+    if (target >= 0) {
+      break;
+    } // No need to continue the loop if it found a proper qualification
+  }
+  let downloadStatus = await fetch(
+    url + "/api/v0/transfers/downloads/" + user,
+    {
+      method: "POST",
+      headers: {
+        "X-API-Key": apiKey,
+      },
+      body: JSON.stringify({
+        filename: directoryDownload(returnArray()[target].files[0].filename),
+      }),
+    },
+  );
+  console.log("Waiting....", 15000);
+}
+async function downloadRYMRequest() {
+  slskdRequest();
+  slskdDownload();
+}
+
+downloadRYMRequest();
