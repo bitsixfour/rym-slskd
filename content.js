5d8db536173de5e4c51492c0d977d0b325128ddb
bitsixfour <tugirasles@gmail.com>
2026-01-01 16:36:22 -0600

    custom theme

--- content.js
+++ content.js
@@ -3,7 +3,12 @@ console.log("Hitting page...");
 console.log("asdfasdf");
 let container = document.querySelector("div.release_my_catalog");
 let button = document.createElement("button");
-
-button.textContent = "slsk";
+button.style.backgroundColor = "#303441";
+button.style.color = "#a9b0bd";
+button.style.fontWeight = "100";
+button.style.borderRadius = "6px";
+button.textContent = "slskd ";
+button.style.height = "34px";
+button.style.outline = "#303441";
 container.appendChild(button);
 console.log(container);
